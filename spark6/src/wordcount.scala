e2d5320c8f30ff982540c39b7798664132acf38a
POORNACHANDAR <POORNACHANDAR@DESKTOP-2ESONUQ>
2020-03-16 12:40:47 +0530

    third commit

--- spark6/src/wordcount.scala
+++ spark6/src/wordcount.scala
@@ -6,6 +6,7 @@ object wordcount {
   
    def main(args:Array[String]){
     
+     println("--------")
     val config=new SparkConf().setAppName("wordcount prog").setMaster("local")
     val sc=new SparkContext(config)
     sc.setLogLevel("ERROR")
@@ -14,11 +15,16 @@ object wordcount {
     val rdd1=rdd.flatMap(x=>x.split(" ")).map(x=>(x,1)).reduceByKey((x,y)=>x+y)
     rdd1.foreach(println)
     
-    println("2nd wordcount")
+    println("--------")
     
      val rdd2=sc.textFile("F:\\data\\wordcount1.txt")
     val rdd3=rdd2.flatMap(x=>x.split(",")).map(x=>(x,1)).reduceByKey((x,y)=>x+y)
      rdd3.foreach(println)
+     
+     println("---------")
+     val rdd4=sc.textFile("F:\\data\\wordcount1.txt")
+     val rdd5=rdd4.map(x=>x.split(",")).map(x=>(x(0),x(1)))
+     rdd5.foreach(println)
     
    }
   
